b75c27cf8a86d168368388d3e96f2b509b41871f
roshan <roshanshah920@gmail.com>
2024-08-31 14:49:57 +0545

    student added

---
+++ Student.java
@@ -0,0 +1,10 @@
+public class Student {
+
+    public static void display(){
+        System.out.println("i am a studnet");
+    }
+    public static void main(String[] args) {
+        display();
+
+    }
+}
